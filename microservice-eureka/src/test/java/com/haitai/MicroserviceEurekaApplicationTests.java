62a7878cd0aebcb8fdca64734ed7606ed9ce8128
bin.wang <bin.wang@haitaichina.com>
2019-08-06 01:13:52 +0800

    创建microservice-eureka工程

---
+++ microservice-eureka/src/test/java/com/haitai/MicroserviceEurekaApplicationTests.java
@@ -0,0 +1,16 @@
+package com.haitai;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class MicroserviceEurekaApplicationTests {
+
+    @Test
+    public void contextLoads() {
+    }
+
+}
