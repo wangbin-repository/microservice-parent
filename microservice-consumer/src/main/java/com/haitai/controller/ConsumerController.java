a436e72380aeb1ab6cdee40ae5677309c189915a
bin.wang <bin.wang@haitaichina.com>
2019-08-27 16:09:48 +0800

    microservice-consumer/src/main/resources/application.yml eureka地址变更 microservice-provider-hystrix/src/main/java/com/haitai/controller/IndexController.java hystrix客户端

--- microservice-consumer/src/main/java/com/haitai/controller/ConsumerController.java
+++ microservice-consumer/src/main/java/com/haitai/controller/ConsumerController.java
@@ -1,6 +1,7 @@
 package com.haitai.controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
@@ -11,12 +12,12 @@ import org.springframework.web.client.RestTemplate;
  */
 @RestController
 public class ConsumerController {
-    private static final String REST_URI_PREFIX = "http://MICROSERVICE-PROVIDER-SYSTEM";
+    private static final String REST_URI_PREFIX = "http://MICROSERVICE-PROVIDER-HYSTRIX";
     @Autowired
     private RestTemplate restTemplate;
 
-    @RequestMapping(value = "/ribbon-consumer", method = RequestMethod.GET)
-    public String getUserInfo() {
-        return restTemplate.getForObject(REST_URI_PREFIX + "/getInstanceId", String.class);
+    @RequestMapping(value = "/ribbon-consumer/{num}", method = RequestMethod.GET)
+    public String getUserInfo(@PathVariable("num") String num) {
+        return restTemplate.getForObject(REST_URI_PREFIX + "/getInstanceId/{num}", String.class, num);
     }
 }
