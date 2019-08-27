b9597e95254719712b903e4f4d8c7bb8592c6bbd
bin.wang <bin.wang@haitaichina.com>
2019-08-27 17:29:26 +0800

    服务功能更新

--- microservice-provider-system/src/main/java/com/haitai/system/controller/IndexController.java
+++ microservice-provider-system/src/main/java/com/haitai/system/controller/IndexController.java
@@ -1,6 +1,7 @@
 package com.haitai.system.controller;
 
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
@@ -13,8 +14,9 @@ public class IndexController {
     @Value("${eureka.instance.instance-id}")
     private String instanceId;
 
-    @RequestMapping(value = "/getInstanceId", method = RequestMethod.GET)
-    public String getInstanceId() {
-        return instanceId;
+    @RequestMapping(value = "/getInstanceId/{num}", method = RequestMethod.GET)
+    public String getInstanceId(@PathVariable("num") String num) {
+        int i = Integer.parseInt(num);
+        return instanceId.toString() + " ->num = " + i;
     }
 }
