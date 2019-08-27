4c5af80ae368482c9f1ee0e41ef58819e588eb9e
bin.wang <bin.wang@haitaichina.com>
2019-08-27 17:28:33 +0800

    microservice-consumer-feign 客户端服务降级

--- microservice-consumer-feign/src/main/java/com/haitai/controller/ConsumerController.java
+++ microservice-consumer-feign/src/main/java/com/haitai/controller/ConsumerController.java
@@ -2,6 +2,7 @@ package com.haitai.controller;
 
 import com.haitai.service.SystemService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
@@ -14,8 +15,8 @@ public class ConsumerController {
     @Autowired
     private SystemService systemService;
 
-    @RequestMapping(value = "/ribbon-consumer", method = RequestMethod.GET)
-    public String getUserInfo() {
-        return systemService.getInstanceId();
+    @RequestMapping(value = "/ribbon-consumer/{num}", method = RequestMethod.GET)
+    public String getUserInfo(@PathVariable("num") String num) {
+        return systemService.getInstanceId(num);
     }
 }
